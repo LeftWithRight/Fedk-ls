afc5b50cf104870a769acae691d7a01845db6919
LeftWithRight <53988519+im-jianhui@users.noreply.github.com>
2023-05-31 18:03:03 +0800

    final

--- reposion.py
+++ reposion.py
@@ -10,6 +10,7 @@ class Args:
         self.num_users = num_users
         self.atk_num = atk_num
         self.frac = frac
+        self.dev = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
 
     def num_none_poison(self):
         print(str(self.num_users - self.atk_num))
@@ -30,50 +31,12 @@ def pearson_sim(param_diff1, param_diff2):
     return similarity
 
 
-# def trainBehavior(blockchain, threshold):
-#     length = len(blockchain)
-#     max_acc = 0
-#
-#     # 获取准确率最好的索引
-#     max_index = None
-#     for i in range(length - 10, length):
-#         if max_acc < blockchain[i].get_accu():
-#             max_acc = blockchain[i].get_accu()
-#             max_index = i
-#     lst = []
-#     thresholdVlaue = threshold * max_acc
-#     print("\n" + 'thresholdVlaue: {}'.format(thresholdVlaue))
-#     for i in range((length-10), length):
-#         if blockchain[i].get_accu() > thresholdVlaue and pearson_sim(blockchain[i].params, blockchain[max_index].params) > threshold:
-#             lst.append(i)
-#     print("参与此次模型聚合的局部模型数量：" + str(len(lst)))
-#     distances = defaultdict(dict)
-#     non_malicious_count = len(lst)
-#     num = 0
-#     w = []
-#     for i in lst:
-#         w.append(blockchain[i].get_para())
-#     num_models = len(w)
-#     for k in w[0].keys():
-#         if num == 0:
-#             for i in range(len(w)):
-#                 for j in range(i):
-#                     distances[i][j] = distances[j][i] = np.linalg.norm(w[i][k].cpu().numpy() - w[j][k].cpu().numpy())
-#             num = 1
-#         else:
-#             for i in range(len(w)):
-#                 for j in range(i):
-#                     distances[j][i] += np.linalg.norm(w[i][k].cpu().numpy() - w[j][k].cpu().numpy())
-#                     distances[i][j] += distances[j][i]
-#     minimal_error = 1e20
-#     minimal_error_index = 0
-#     for user in distances.keys():
-#         errors = sorted(distances[user].values())
-#         current_error = sum(errors[:non_malicious_count])
-#         if current_error < minimal_error:
-#             minimal_error = current_error
-#             minimal_error_index = user
-#     return w[minimal_error_index]
+def fedavg(blockchain):
+    length = len(blockchain)
+
+
+
+
 
 def trainBehavior(blockchain, threshold):
     length = len(blockchain)
@@ -96,32 +59,56 @@ def trainBehavior(blockchain, threshold):
         if blockchain[i].get_accu() > thresholdVlaue and pearson_sim(blockchain[i].params, blockchain[max_index].params) > threshold:
             lst.append(i)
     print("参与此次模型聚合的局部模型数量：" + str(len(lst)), "区块链中的下标", lst)
+    print(lst)
+    sum_parameters = None
     w = []
     for i in lst:
         w.append(blockchain[i].params)
     num_models = len(lst)
-    num_non_malicious = num_models
-    distances = np.zeros((num_models, num_models))
-    for i in range(num_models):
-        for j in range(i):
-            dist = 0
-            for param_name, param_value in w[i].items():
-                t = param_value.cpu().numpy()
-                t1 = w[j][param_name].cpu().numpy()
-                dist += np.linalg.norm(t - t1)
-                # dist += torch.norm(param_value - w[j][param_name]).item()
-            distances[i, j] = distances[j, i] = dist
+    # 对所有的Cluster返回的参数累加（最后取平均值）
+    for local_parameters in w:
+        if sum_parameters is None:
+            sum_parameters = {}
+            for key, var in local_parameters.items():
+                sum_parameters[key] = var.clone()
+        else:
+            for var in sum_parameters:
+                sum_parameters[var] = sum_parameters[var] + local_parameters[var]
+    # 取平均值
+    global_parameters = copy.deepcopy(sum_parameters)
+    for var in global_parameters:
+        global_parameters[var] = (sum_parameters[var] / num_models)
+    return global_parameters
 
-    sorted_distances = np.sort(distances, axis=1)
-    selected_distances = sorted_distances[:, :num_non_malicious]
 
-    # 计算选择的距离之和
-    errors = np.sum(selected_distances, axis=1)
 
-    # 找到最小错误值对应的模型索引
-    krum_index = np.argmin(errors)
 
-    return w[krum_index]
+
+
+
+    # num_non_malicious = num_models
+    # distances = np.zeros((num_models, num_models))
+    # for i in range(num_models):
+    #     for j in range(i):
+    #         dist = 0
+    #         for param_name, param_value in w[i].items():
+    #             t = param_value.cpu().numpy()
+    #             t1 = w[j][param_name].cpu().numpy()
+    #             dist += np.linalg.norm(t - t1)
+    #             # dist += torch.norm(param_value - w[j][param_name]).item()
+    #         distances[i, j] = distances[j, i] = dist
+    #
+    # sorted_distances = np.sort(distances, axis=1)
+    # selected_distances = sorted_distances[:, :num_non_malicious]
+    #
+    # # 计算选择的距离之和
+    # errors = np.sum(selected_distances, axis=1)
+    #
+    # # 找到最小错误值对应的模型索引
+    # krum_index = np.argmin(errors)
+    # print(krum_index)
+    #
+    # return w[krum_index]
 
 
 def krum(blockchain, args):  # w中存储的是所有分簇的模型参数 args存储标签反转数据信息
@@ -181,28 +168,56 @@ def medium(blockchain, args):
 
 
 def trimmed_mean(blockchain, args):
+    number_to_consider = int(args.num_users - 2)
+    print(number_to_consider)
     length = len(blockchain)
     w = []
-    for i in range(length - 10, length):
+    for i in range(length-10, length):
         w.append(blockchain[i].get_para())
-    number_to_consider = int((args.num_users - args.atk_num) * args.frac) - 1
-    print(number_to_consider)
     w_avg = copy.deepcopy(w[0])
     for k in w_avg.keys():
         tmp = []
         for i in range(len(w)):
-            tmp.append(w[i][k].numpy()) # get the weight of k-layer which in each client
+            tmp.append(w[i][k].cpu().numpy()) # get the weight of k-layer which in each client
         tmp = np.array(tmp)
-        med = np.median(tmp, axis=0)
+        med = np.median(tmp,axis=0)
         new_tmp = []
+
         for i in range(len(tmp)):             # cal each client weights - median
             new_tmp.append(tmp[i]-med)
         new_tmp = np.array(new_tmp)
-        good_vals = np.argsort(np.abs(new_tmp), axis=0)[:number_to_consider]
+        good_vals = np.argsort(abs(new_tmp),axis=0)[:number_to_consider]
         good_vals = np.take_along_axis(new_tmp, good_vals, axis=0)
         k_weight = np.array(np.mean(good_vals) + med)
-        w_avg[k] = torch.from_numpy(k_weight)
+        w_avg[k] = torch.from_numpy(k_weight).to(args.dev)
     return w_avg
+
+
+
+# def trimmed_mean(blockchain, args):
+#     length = len(blockchain)
+#     w = []
+#     for i in range(length - 10, length):
+#         w.append(blockchain[i].get_para())
+#     number_to_consider = args.num_users - args.atk_num
+#     print(number_to_consider)
+#     w_avg = copy.deepcopy(w[0])
+#     for k in w_avg.keys():
+#         tmp = []
+#         for i in range(len(w)):
+#             tmp.append(w[i][k].numpy()) # get the weight of k-layer which in each client
+#         tmp = np.array(tmp)
+#         med = np.median(tmp, axis=0)
+#         new_tmp = []
+#         for i in range(len(tmp)):             # cal each client weights - median
+#             new_tmp.append(tmp[i]-med)
+#
+#         new_tmp = np.array(new_tmp)
+#         good_vals = np.argsort(np.abs(new_tmp), axis=0)[:number_to_consider]
+#         good_vals = np.take_along_axis(new_tmp, good_vals, axis=0)
+#         k_weight = np.array(np.mean(good_vals) + med)
+#         w_avg[k] = torch.from_numpy(k_weight)
+#     return w_avg
 
 
 # def trimmed_mean(blockchain):
--- server.py
+++ server.py
@@ -42,9 +42,9 @@ parser.add_argument('-iid', '--IID', type=int, default=1, help='the way to alloc
 parser.add_argument('-threshold', '--thresholdValue', type=float, default=0.8, help='set the thresValue in repoison')
 parser.add_argument('-dp', '--dropout', type=float, default=0.1, help='set the dropout level in net')
 parser.add_argument('-op', '--opti', type=str, default='SGD', help='set the opti in net')
-parser.add_argument('-poipro', '--poisonprob', type=float, default=0.7, help='poison data')
+parser.add_argument('-poipro', '--poisonprob', type=float, default=0.8, help='poison data')
 parser.add_argument('-revprob', '--reverseprob', type=float, default=0.4, help='set the reverprob')
-parser.add_argument('-repoi', '--repoisonalgorithms', type=str, default='trainBehavior', help='set the repoison algorithms')
+parser.add_argument('-repoi', '--repoisonalgorithms', type=str, default='trimmed_mean', help='set the repoison algorithms')
 
 
 
